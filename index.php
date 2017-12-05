7e2402b5dfa437f43b7f438df53ee058d57ea270
Giildo <giildo.jm@gmail.com>
2017-12-05 13:54:20 +0100

    Ajout du dossier images pour les différents films + création de la fonction JQuery pour changer les images toutes les 2sec + Ajout des images dans le squelette HTML

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 <head>
    <title>Festival de Films en Plein Air</title>
    <meta charset="utf-8" />
-   <link rel="stylesheet" type="text/css" href="css/style.css" />
    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+   <link rel="stylesheet" type="text/css" href="style.css" />
 </head>
 <body>
 
@@ -32,14 +32,43 @@
    </nav>
 </header>
 
-<article>
-   Du texte pour essayer
-</article>
+<section class="container-fluid">
+   <div class="row">
+      <a href="#"><img src="images/2001_lodyssee_de_lespace/1.jpg" class="col-md-2 col-md-offset-3" id="img1" data-film="2001_lodyssee_de_lespace" /></a>
+      <a href="#"><img src="images/2001_lodyssee_de_lespace/1.jpg" class="col-md-2" id="img2" /></a>
+      <a href="#"><img src="images/2001_lodyssee_de_lespace/1.jpg" class="col-md-2" id="img3" /></a>
+   </div>
+</section>
 
 <footer>
    <p>&copy Les films en Plein Air - Site réalisé par <a href="http://www.jojotique.fr">Jojotique</a></p>
 </footer>
 
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<script
+  src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+  integrity="sha256-k2WSCIexGzOj3Euiig+TlR8gA0EmPjuc79OEeY5L45g="
+  crossorigin="anonymous"></script>
+<script
+   src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+   integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+   crossorigin="anonymous"></script>
+
+<script type="text/javascript">
+$(function(){
+   var i = 0;
+   var cheminFilm = 'images/' + $('#img1').data('film');
+
+   setInterval(function(){
+      if(i >= 6)
+      {
+         i = 1;
+      } else {
+         i++;
+      }
+
+      $('#img1').attr('src', cheminFilm + '/' + i + '.jpg');
+   }, 2000);
+})
+</script>
 </body>
 </html>
