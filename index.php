c3e88c4ce694c970ca78cc5197245a8ddf40b971
Giildo <giildo.jm@gmail.com>
2017-12-05 10:33:12 +0100

    Création du projet P3 et initialisation du Git

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="Fr">
+<head>
+   <title>Festival de Films en Plein Air</title>
+   <meta charset="utf-8" />
+   <link rel="stylesheet" type="text/css" href="css/style.css" />
+   <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+</head>
+<body>
+
+<header>
+   <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container-fluid">
+         <div class="navbar-header">
+            <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#jojotique_menu">
+               <span class="icon-bar"></span>
+               <span class="icon-bar"></span>
+               <span class="icon-bar"></span>
+            </button>
+            <a href="#" class="navbar-brand">Festival de Films en Plein Air</a>
+         </div>
+         <div class="collapse navbar-collapse" id="jojotique_menu">
+            <ul class="nav navbar-nav navbar-right">
+               <li><a href="#">Le festival</a></li>
+               <li><a href="#">La programmation</a></li>
+               <li><a href="#">Actualités</a></li>
+               <li><a href="#">Nous connaitre</a></li>
+            </ul>
+         </div>
+      </div>
+
+   </nav>
+</header>
+
+<article>
+   Du texte pour essayer
+</article>
+
+<footer>
+   <p>&copy Les films en Plein Air - Site réalisé par <a href="http://www.jojotique.fr">Jojotique</a></p>
+</footer>
+
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</body>
+</html>
